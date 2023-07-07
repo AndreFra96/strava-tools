607a4fb074dd5dd79aab67677e3de53ae44e0720
Andrea Brioschi <brioschiandrea@tiscali.it>
2023-07-07 11:46:39 +0200

    aggiunta SDK garmin per manipolazione file fit e preparata funzione di lettura #5

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,6 @@
+import fs from "fs";
+import { Decoder, Stream } from "./lib/garmin-fit-sdk/src";
+
 async function codeForTokenExchange(
   client_id: string,
   client_secret: string,
@@ -23,15 +26,18 @@ async function codeForTokenExchange(
   return data;
 }
 
-async function getAthleteStats(token_access: string, id_atleta: string){
+async function getAthleteStats(token_access: string, id_atleta: string) {
   const config = {
     headers: {
-      "Authorization": "Bearer " + token_access
-    }
-  }
+      Authorization: "Bearer " + token_access,
+    },
+  };
 
-  const response = await fetch(`https://www.strava.com/api/v3/athletes/${id_atleta}/stats`,config)
-  const data = await response.json()
+  const response = await fetch(
+    `https://www.strava.com/api/v3/athletes/${id_atleta}/stats`,
+    config
+  );
+  const data = await response.json();
 
   return data;
 }
@@ -39,16 +45,55 @@ async function getAthleteStats(token_access: string, id_atleta: string){
 async function getAthleteActivies(token_access: string) {
   const config = {
     headers: {
-      "Authorization": "Bearer " + token_access
-    }
+      Authorization: "Bearer " + token_access,
+    },
   };
 
-  const response = await fetch(`https://www.strava.com/api/v3/athlete/activities`,config)
-  const data = await response.json()
+  const response = await fetch(
+    `https://www.strava.com/api/v3/athlete/activities`,
+    config
+  );
+  const data = await response.json();
 
   return data;
 }
 
-export { codeForTokenExchange, getAthleteStats, getAthleteActivies};
+type DecodedFit = {
+  messages: Record<string, any>;
+  errors: any[];
+};
 
+/**
+ * Decodifica un file .fit restituendo il contenuto in formato JSON
+ * @param filePath file .fit da decodificare
+ * @returns contenuto del file .fit decodificato
+ */
+async function decodeFit(
+  filePath: fs.PathOrFileDescriptor
+): Promise<DecodedFit> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      const stream = Stream.fromBuffer(data);
+      const decoder = new Decoder(stream);
+      const fitData = decoder.read();
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (
+        !fitData.hasOwnProperty("messages") ||
+        !fitData.hasOwnProperty("errors")
+      ) {
+        reject("Errore nella lettura del file FIT");
+        return;
+      }
+      const { messages, errors } = fitData as {
+        messages: Record<string, any>;
+        errors: any[];
+      };
+      resolve({ messages, errors });
+    });
+  });
+}
 
+export { codeForTokenExchange, getAthleteStats, getAthleteActivies, decodeFit };
